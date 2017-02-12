6dbf57d58bc3f9663b014ddd4ae677de5e3ecc83
msqiqi <zzzr0054qnmb@gmail.com>
2017-02-12 16:46:24 -0600

    initial commit

--- softwareprocess/convertString2Dictionary.py
+++ softwareprocess/convertString2Dictionary.py
@@ -1,5 +1,37 @@
 import urllib
+import re
+from collections import OrderedDict
 
-def convertString2Dictionary(inputString=""):
-    errorDict = {'error':'true'}
-    return errorDict
+
+def convertString2Dictionary(inputString = ""):
+    errorDict = {'error': 'true'}
+    untestedString = urllib.unquote(inputString)
+    #check if string is empty
+    if untestedString == '':
+        return errorDict
+    #check string content
+    if not re.match("^[A-Za-z0-9=.,\s]*$", untestedString):
+        return errorDict
+    untestedStringList = untestedString.split(',')
+    #put pairs into dict and check blank, duplication, missing key or value, if key is start with alphabet
+    orderedList = []
+    dic1 = {}
+    for subString in untestedStringList:
+        kvpair = subString.split('=')
+        if not len(kvpair) == 2:
+            return errorDict
+        keyVal = kvpair[0].strip()
+        value = kvpair[1].strip()
+        if ' ' in keyVal or ' ' in value:
+            return errorDict
+        if keyVal == '' or value == '':
+            return errorDict
+        if not keyVal[0].isalpha():
+            return errorDict
+        for key in dic1.keys():
+            if keyVal == key:
+                return errorDict
+        orderedList.append((keyVal, value))
+        dic1[keyVal] = value
+    dic = OrderedDict(orderedList)
+    return dic
